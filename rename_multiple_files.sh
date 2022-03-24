d231ff3d478b91f2f16e05be368816bcc5d8ae68
Rohit Gupta <64639204+rohitgupta29@users.noreply.github.com>
2022-03-24 21:55:19 +0530

    Create rename_multiple_files.sh

---
+++ rename_multiple_files.sh
@@ -0,0 +1,87 @@
+cd ./student
+mv v1_texas_st_ext_crisisevent.csv v3_texas_st_ext_crisisevent.csv
+mv v1_texas_st_ext_censusblockgroup.csv v3_texas_st_ext_censusblockgroup.csv
+mv v1_student_visa.csv v3_student_visa.csv
+mv v1_student_personalidentificationdoc.csv v3_student_personalidentificationdoc.csv
+mv v1_student_othername.csv v3_student_othername.csv
+mv v1_student_identificationdoc.csv v3_student_identificationdoc.csv
+mv v1_student_base.csv v3_student_base.csv
+
+cd ../ssa
+mv v1_ssa_educationplan.csv  v3_ssa_educationplan.csv  
+mv v1_ssa_base.csv  v3_ssa_base.csv  
+mv v1_ssa_agp.csv  v3_ssa_agp.csv  
+
+cd ../spa
+
+mv v1_spa_service.csv v3_spa_service.csv  
+mv v1_spa_generalspaprgpartcpnstatus.csv v3_spa_generalspaprgpartcpnstatus.csv 
+mv v1_spa_base.csv v3_spa_base.csv 
+
+cd ../seoa
+
+mv v1_texas_seoa_ext_unschooledasyleerefugee.csv v3_texas_seoa_ext_unschooledasyleerefugee.csv
+mv v1_texas_seoa_ext_unaccompaniedyouth.csv v3_texas_seoa_ext_unaccompaniedyouth.csv
+mv v1_texas_seoa_ext_pkprogramtype.csv v3_texas_seoa_ext_pkprogramtype.csv
+mv v1_texas_seoa_ext_pkfundingsource.csv v3_texas_seoa_ext_pkfundingsource.csv
+mv v1_texas_seoa_ext_militaryconnectedstudent.csv v3_texas_seoa_ext_militaryconnectedstudent.csv
+mv v1_texas_seoa_ext_homelessstatus.csv v3_texas_seoa_ext_homelessstatus.csv
+mv v1_texas_seoa_ext_fostercaretype.csv v3_texas_seoa_ext_fostercaretype.csv
+mv v1_texas_seoa_ext_economicdisadvantage.csv v3_texas_seoa_ext_economicdisadvantage.csv
+mv v1_texas_seoa_ext_earlyreadingindicator.csv v3_texas_seoa_ext_earlyreadingindicator.csv
+mv v1_texas_seoa_ext_dyslexiaservice.csv v3_texas_seoa_ext_dyslexiaservice.csv
+mv v1_texas_seoa_ext_dyslexiarisk.csv v3_texas_seoa_ext_dyslexiarisk.csv
+mv v1_seoa_tribalaffiliation.csv v3_seoa_tribalaffiliation.csv
+mv v1_seoa_telephone.csv v3_seoa_telephone.csv
+mv v1_seoa_stindicator_stindicatorperiod.csv v3_seoa_stindicator_stindicatorperiod.csv
+mv v1_seoa_stindicator.csv v3_seoa_stindicator.csv
+mv v1_seoa_stidentificationcode.csv v3_seoa_stidentificationcode.csv
+mv v1_seoa_stchar_stcharperiod.csv v3_seoa_stchar_stcharperiod.csv
+mv v1_seoa_stchar.csv v3_seoa_stchar.csv
+mv v1_seoa_race.csv v3_seoa_race.csv
+mv v1_seoa_prgpartcpn_prgpartcpnprgchar.csv v3_seoa_prgpartcpn_prgpartcpnprgchar.csv
+mv v1_seoa_prgpartcpn.csv v3_seoa_prgpartcpn.csv
+mv v1_seoa_language_languageuse.csv v3_seoa_language_languageuse.csv
+mv v1_seoa_language.csv v3_seoa_language.csv
+mv v1_seoa_internationaladdress.csv v3_seoa_internationaladdress.csv
+mv v1_seoa_electronicmail.csv v3_seoa_electronicmail.csv
+mv v1_seoa_disability_disabilitydesignation.csv v3_seoa_disability_disabilitydesignation.csv
+mv v1_seoa_disability.csv v3_seoa_disability.csv
+mv v1_seoa_cohortyear.csv v3_seoa_cohortyear.csv
+mv v1_seoa_base.csv v3_seoa_base.csv
+mv v1_seoa_ancestryethnicorigin.csv v3_seoa_ancestryethnicorigin.csv
+mv v1_seoa_address.csv v3_seoa_address.csv
+mv v1_seoa_address_addressperiod.csv v3_seoa_address_addressperiod.csv
+
+cd ../school
+
+mv v1_texas_school_ext_nslptype.csv v3_texas_school_ext_nslptype.csv
+mv v1_texas_school_ext_elotype_elotypeeloactivity.csv v3_texas_school_ext_elotype_elotypeeloactivity.csv
+mv v1_texas_school_ext_elotype.csv v3_texas_school_ext_elotype.csv
+mv v1_texas_school_ext_charterwaitlist.csv v3_texas_school_ext_charterwaitlist.csv
+mv v1_texas_school_ext_campusenrollmenttype.csv v3_texas_school_ext_campusenrollmenttype.csv
+mv v1_school_gradelevel.csv v3_school_gradelevel.csv
+mv v1_school_edorginternationaladdr.csv v3_school_edorginternationaladdr.csv
+mv v1_school_edorginstitutiontelephone.csv v3_school_edorginstitutiontelephone.csv
+mv v1_school_edorgindicator_period.csv v3_school_edorgindicator_period.csv
+mv v1_school_edorgindicator.csv v3_school_edorgindicator.csv
+mv v1_school_edorgidentificationcode.csv v3_school_edorgidentificationcode.csv
+mv v1_school_edorgcategory.csv v3_school_edorgcategory.csv
+mv v1_school_edorgaddr_period.csv v3_school_edorgaddr_period.csv
+mv v1_school_edorgaddr.csv v3_school_edorgaddr.csv
+mv v1_school_category.csv v3_school_category.csv
+mv v1_school_base.csv v3_school_base.csv
+
+cd ../program
+
+mv v1_program_sponsor.csv v3_program_sponsor.csv
+mv v1_program_service.csv v3_program_service.csv
+mv v1_program_learningstd.csv v3_program_learningstd.csv
+mv v1_program_learningobjective.csv v3_program_learningobjective.csv
+mv v1_program_characteristic.csv v3_program_characteristic.csv
+mv v1_program_base.csv v3_program_base.csv
+
+cd ../calendar
+
+mv v1_calendar_gradelevel.csv v3_calendar_gradelevel.csv
+mv v1_calendar_base.csv v3_calendar_base.csv
