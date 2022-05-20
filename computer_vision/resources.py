8e10003f21c9f7dae4aeb4a8e0a826b96238e600
Rohit Gupta <64639204+rohitgupta29@users.noreply.github.com>
2022-05-20 16:56:57 +0530

    Create resources.py

---
+++ computer_vision/resources.py
@@ -0,0 +1,9 @@
+1. https://medium.com/saarthi-ai/how-to-build-your-own-ocr-a5bb91b622ba 
+  
+2. https://github.com/sourabh-suri/Pan-Card-OCR 
+  
+3. https://github.com/008karan/PAN_OCR
+  
+4. https://jonathan-hui.medium.com/object-detection-speed-and-accuracy-comparison-faster-r-cnn-r-fcn-ssd-and-yolo-5425656ae359 
+  
+5. https://medium.com/nerd-for-tech/aadhaar-pan-info-extraction-using-python-ocr-5df81b6c66e3
